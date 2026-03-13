5098c5c951875f2eb0292d6ac9b49d5a6b612444
Matthias C. M. Troffaes <matthias.troffaes@gmail.com>
2026-03-13 14:57:39 +0000

    lua: add T.validate

--- lua/mempeep/T.lua
+++ lua/mempeep/T.lua
@@ -39,5 +39,45 @@ T.vector = function(element_type_ref)
     return {kind = "vector", type_ref = element_type_ref}
 end
 
+local function validate(type_ref)
+    if not type_ref.kind then
+        error("type_ref has no kind")
+    end
+    if type_ref.kind == "array" then
+        assert(type_ref.type_ref, "array has no type_ref")
+        assert(type_ref.count, "array has no count")
+        assert(type_ref.count >= 1, "array count must be at least 1")
+        validate(type_ref.type_ref)
+    elseif type_ref.kind == "circular_list" then
+        assert(type_ref.type_ref, "circular_list has no type_ref")
+        validate(type_ref.type_ref)
+    elseif type_ref.kind == "float" then
+        assert(true)
+    elseif type_ref.kind == "i8" then
+        assert(true)
+    elseif type_ref.kind == "i16" then
+        assert(true)
+    elseif type_ref.kind == "i32" then
+        assert(true)
+    elseif type_ref.kind == "i64" then
+        assert(true)
+    elseif type_ref.kind == "ptr" then
+        assert(type_ref.type_ref, "ptr has no type_ref")
+        validate(type_ref.type_ref)
+    elseif type_ref.kind == "string" then
+        assert(type_ref.max_length, "string has no max_length")
+        assert(type_ref.max_length >= 1, "string max_length must be at least 1")
+    elseif type_ref.kind == "struct" then
+        assert(type_ref.name, "struct type_ref has no name")
+    elseif type_ref.kind == "vector" then
+        assert(type_ref.type_ref, "vector has no type_ref")
+        validate(type_ref.type_ref)
+    else
+        error("unknown type_ref kind: " .. type_ref.kind)
+    end
+end
+
+T.validate = validate
+
 return T
 
--- lua/test.lua
+++ lua/test.lua
@@ -1,14 +1,30 @@
 local T = require("mempeep.T")
 
-assert(T.array(T.i32, 5)["kind"] == "array", "unexpected kind for T.array")
-assert(T.array(T.i32, 5)["type_ref"] == T.i32, "unexpected type_ref for T.array")
-assert(T.array(T.i32, 5)["count"] == 5, "unexpected count for T.array")
-assert(T.circular_list(T.struct("Item"))["kind"] == "circular_list", "unexpected kind for T.circular_list")
+local arr = T.array(T.i32, 5)
+T.validate(arr)
+assert(arr["kind"] == "array", "unexpected kind for T.array")
+assert(arr["type_ref"] == T.i32, "unexpected type_ref for T.array")
+assert(arr["count"] == 5, "unexpected count for T.array")
+
+local stru = T.struct("Item")
+T.validate(stru)
+assert(stru["kind"] == "struct", "unexpected kind for T.struct")
+assert(stru["name"] == "Item", "unexpected name for T.struct")
+
+local circ = T.circular_list(stru)
+T.validate(circ)
+assert(circ["kind"] == "circular_list", "unexpected kind for T.circular_list")
+assert(circ["type_ref"] == stru, "unexpected type_ref for T.circular_list")
+
 assert(T.i8["kind"] == "i8", "unexpected kind for T.i8")
-assert(T.optional_ref(T.i32)["kind"] == "ptr", "unexpected kind for T.optional_ref")
-assert(T.optional_ref(T.i32)["type_ref"] == T.i32, "unexpected ref_type for T.optional_ref")
-assert(T.struct("Item")["kind"] == "struct", "unexpected kind for T.struct")
-assert(T.struct("Item")["name"] == "Item", "unexpected name for T.struct")
-assert(T.vector(T.i32)["kind"] == "vector", "unexpected kind for T.vector")
-assert(T.vector(T.i32)["type_ref"] == T.i32, "unexpected type_ref for T.vector")
+
+local opt_ref = T.optional_ref(T.i32)
+T.validate(opt_ref)
+assert(opt_ref["kind"] == "ptr", "unexpected kind for T.optional_ref")
+assert(opt_ref["type_ref"] == T.i32, "unexpected ref_type for T.optional_ref")
+
+local vec = T.vector(T.i32)
+T.validate(vec)
+assert(vec["kind"] == "vector", "unexpected kind for T.vector")
+assert(vec["type_ref"] == T.i32, "unexpected type_ref for T.vector")
 
