25bf8e1f595af852894a9bf9dbf48522ebda1a74
Matthias C. M. Troffaes <matthias.troffaes@gmail.com>
2026-03-15 17:52:07 +0000

    Some renames and clarifications in docs.

--- cpp/mempeep/D/field_type.hpp
+++ cpp/mempeep/D/field_type.hpp
@@ -13,21 +13,23 @@ namespace mempeep::detail {
 
 	// Field specialization: assign T if not void,
 	// or otherwise extract the underlying type from the member pointer M.
-	template<auto M, typename T>
-	struct field_type<D::Field<M, T>> {
+	template<auto MemberPtr, typename T>
+	struct field_type<D::Field<MemberPtr, T>> {
 		using type = std::conditional_t<
 			std::is_void_v<T>,
 			// if T = void, return type of member pointer
 			// Remember M is here the member pointer itself, not the type of the member pointer.
 			// So we need decltype(M) as member_ptr_type_t expects the type of the member pointer.
-			member_ptr_type_t<decltype(M)>,
+			member_ptr_type_t<decltype(MemberPtr)>,
 			// otherwise return T
 			T
 		>;
 	};
 
 	// Shorthand.
-	template<typename F>
-	using field_type_t = typename field_type<F>::type;
+	// field_type_t<T> is a shorthand for field_type<T>::type.
+	// Only use if T is a member pointer, such as &Pos::x.
+	template<typename T>
+	using field_type_t = typename field_type<T>::type;
 
 }
--- cpp/mempeep/D/member_ptr_type.hpp
+++ cpp/mempeep/D/member_ptr_type.hpp
@@ -7,19 +7,19 @@ namespace mempeep::detail {
 	struct member_ptr_type;
 
 	// Member pointer type specialization: extract underlying type of member.
-	// "T C::*" is the type of a pointer to a member of the class C of type T.
+	// "Type Class::*" is the type of a pointer to a member of type Type in class Class.
 	// For instance, "int Pos::*" is the type of &Pos::x if x is defined as an int member of Pos.
 	// You can get this type using decltype(&Pos::x).
-	template<typename C, typename T>
-	struct member_ptr_type<T C::*> {
-		using type = T;
+	template<typename Class, typename MemberType>
+	struct member_ptr_type<MemberType Class::*> {
+		using type = MemberType;
 	};
 
-	// Shorthand for convenience.
-	// member_ptr_type_t<MP> is a shorthand for member_ptr_type<MP>::type.
-	// Only to be used if MP is a member pointer type.
-	template<typename MP>
+	// Shorthand.
+	// member_ptr_type_t<T> is a shorthand for member_ptr_type<T>::type.
+	// Only use if T is a member pointer type, such as decltype(&Pos::x).
+	template<typename T>
 	using member_ptr_type_t =
-		typename member_ptr_type<MP>::type;
+		typename member_ptr_type<T>::type;
 
 }
