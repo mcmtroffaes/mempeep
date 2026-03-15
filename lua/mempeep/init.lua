57ebdf98b57c2874802abd8453da4b11737aa544
Matthias C. M. Troffaes <matthias.troffaes@gmail.com>
2026-03-15 10:10:34 +0000

    Support require("mempeep").

---
+++ lua/mempeep/init.lua
@@ -0,0 +1,10 @@
+local mempeep = {}
+
+mempeep.T = require("mempeep.T")
+mempeep.D = require("mempeep.D")
+mempeep.schema = require("mempeep.schema")
+mempeep.reader = require("mempeep.reader")
+mempeep.printer = require("mempeep.printer")
+mempeep.decoder = require("mempeep.decoder")
+
+return mempeep
