39e0fcfe3d394780450503492b4e8534d685f434
Matthias C. M. Troffaes <matthias.troffaes@gmail.com>
2026-03-16 22:53:35 +0000

    Add some documentation, improve consistency in argument ordering.

--- cpp/test/example.cpp
+++ cpp/test/example.cpp
@@ -5,8 +5,10 @@
 #include <functional>
 #include <type_traits>
 
+namespace mempeep {
+
 // ============================================================
-// Public API
+// Public API: Layout & ReadRemote
 // ============================================================
 
 template <auto Member>
@@ -21,13 +23,41 @@ struct Offset {};
 template <typename... Items>
 struct Layout {};
 
+/**
+ * @brief Template for registering the remote layout of a native struct.
+ *
+ * To be specialized by the user for each struct with non-native remote layout.
+ * Example:
+ *
+ *   template <>
+ *   struct Remote<Pos> {
+ *     using layout = Layout<Field<&Pos::x>, Field<&Pos::y>, Field<&Pos::z>>;
+ *   };
+};
+ */
 template <typename T>
-struct Remote;  // user specialization
+struct Remote;
 
-// Reads memory of given size into buffer (if not null),
-// and returns cursor advanced by size.
-// Caller is responsible for pointer validation, handling overflows, etc.
-using ReadRemote = std::function<intptr_t(void*, intptr_t, intptr_t)>;
+/**
+ * @brief Copy remote memory to a native buffer and advance remote pointer.
+ *
+ * To be implemented by the user.
+ * Reads memory of given size into buffer (if not null),
+ * and returns cursor advanced by size.
+ * Responsible for pointer validation, handling overflows, etc.
+ *
+ * @param cursor Source location in remote memory.
+ * @param size Number of bytes to copy.
+ * @param buffer The native destination buffer.
+ * @return An integer result after processing.
+ */
+using ReadRemote = std::function<intptr_t(intptr_t, intptr_t, void*)>;
+
+// Forward declaration to support recursive Layout reading.
+template <typename T>
+intptr_t read(const ReadRemote& read_remote, intptr_t base, T& out);
+
+namespace detail {
 
 // ============================================================
 // Helpers
@@ -51,70 +81,84 @@ inline constexpr bool has_remote_v = false;
 template <typename T>
 inline constexpr bool has_remote_v<T, std::void_t<remote_layout_t<T>>> = true;
 
-// ============================================================
-// read
-// ============================================================
-
-// forward declaration for recursive read in Field
-template <typename T>
-intptr_t read(T& out, const ReadRemote& read_remote, intptr_t base);
-
 // if Item is not specialized then we must have an invalid item in Layout
 template <typename T, typename Item>
-intptr_t apply_read_rule(Item, T&, const ReadRemote&, intptr_t, intptr_t) {
+intptr_t apply_read_rule(Item, const ReadRemote&, intptr_t, intptr_t, T&) {
   static_assert(!std::is_same_v<Item, Item>, "Unsupported layout item");
 }
 
+// Specialization for Pad<N>.
 template <typename T, std::size_t N>
 intptr_t apply_read_rule(
-  Pad<N>, T&, const ReadRemote& read_remote, intptr_t, intptr_t cursor
+  Pad<N>, const ReadRemote& read_remote, intptr_t, intptr_t cursor, T&
 ) {
-  return read_remote(nullptr, cursor, N);
+  return read_remote(cursor, N, nullptr);
 }
 
+// Specialization for Offset<N>.
 template <typename T, std::size_t N>
 constexpr intptr_t apply_read_rule(
-  Offset<N>, T&, const ReadRemote& read_remote, intptr_t base, intptr_t
+  Offset<N>, const ReadRemote& read_remote, intptr_t base, intptr_t, T&
 ) {
-  return read_remote(nullptr, base, N);
+  return read_remote(base, N, nullptr);
 }
 
+// Specialization for Field<Member>.
 template <typename T, auto Member>
 intptr_t apply_read_rule(
   Field<Member>,
-  T& out,
   const ReadRemote& read_remote,
   intptr_t base,
-  intptr_t cursor
+  intptr_t cursor,
+  T& out
 ) {
   using member_type = typename member_pointer_traits<Member>::member_type;
   auto& field = out.*Member;
   if constexpr (has_remote_v<member_type>) {
     // read returns the new cursor after the nested struct
-    return read(field, read_remote, cursor);
+    return read(read_remote, cursor, field);
   } else {
-    return read_remote(&field, cursor, sizeof(field));
+    return read_remote(cursor, sizeof(field), &field);
   }
 }
 
 template <typename T, typename... Items>
 intptr_t read_layout(
-  Layout<Items...>, T& out, const ReadRemote& read_remote, intptr_t base
+  Layout<Items...>, const ReadRemote& read_remote, intptr_t base, T& out
 ) {
   intptr_t cursor = base;
   // fold from first to last item
-  ((cursor = apply_read_rule(Items{}, out, read_remote, base, cursor)), ...);
+  ((cursor = apply_read_rule(Items{}, read_remote, base, cursor, out)), ...);
   return cursor;
 }
 
-template <typename T>
-intptr_t read(T& out, const ReadRemote& read_remote, intptr_t base) {
-  static_assert(has_remote_v<T>, "Remote<T> must be specialized");
-  return read_layout(remote_layout_t<T>{}, out, read_remote, base);
-}
+}  // namespace detail
 
 // ============================================================
-// example
+// Public API: read
+// ============================================================
+
+/**
+ * @brief Read native type T from remote memory, using Remote<T>::layout if
+ * needed.
+ *
+ * @param read_remote Function for reading bytes from remote memory.
+ * @param base        Remote base pointer of the struct.
+ * @param out         Native destination.
+ * @return            Remote pointer to the memory right after.
+ */
+template <typename T>
+intptr_t read(const ReadRemote& read_remote, intptr_t base, T& out) {
+  static_assert(detail::has_remote_v<T>, "Remote<T> must be specialized");
+  return detail::read_layout(
+    detail::remote_layout_t<T>{}, read_remote, base, out
+  );
+}
+
+}  // namespace mempeep
+
+// ============================================================
+// Example
 // ============================================================
 
 #include <iostream>
@@ -123,7 +167,7 @@ template <intptr_t N>
 struct ReadRemoteMock {
   char data[N]{};
 
-  intptr_t operator()(void* buffer, intptr_t cursor, intptr_t size) const {
+  intptr_t operator()(intptr_t cursor, intptr_t size, void* buffer) const {
     // handle overflow/underflow first
     assert(
       size >= 0 && size < N && cursor >= 0 && cursor < N && size <= N - cursor
@@ -148,12 +192,12 @@ struct Player {
 };
 
 template <>
-struct Remote<Pos> {
+struct mempeep::Remote<Pos> {
   using layout = Layout<Field<&Pos::x>, Pad<4>, Field<&Pos::y>, Pad<4>>;
 };
 
 template <>
-struct Remote<Player> {
+struct mempeep::Remote<Player> {
   using layout = Layout<
     Offset<8>,
     Field<&Player::health>,
@@ -168,7 +212,7 @@ int main() {
   buf.write_int(34, 22);
 
   Player player{};
-  read(player, buf, 10);
+  mempeep::read(buf, 10, player);
 
   assert(player.health == 123);
   assert(player.pos.x == 11);
