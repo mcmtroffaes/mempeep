c4ef73689cc0afdb5752ed4af9c129ccb961bb3a
Matthias C. M. Troffaes <matthias.troffaes@gmail.com>
2026-03-16 16:40:57 +0000

    Simplify read so it no longer needs remote_size.

--- cpp/test/example.cpp
+++ cpp/test/example.cpp
@@ -64,123 +64,70 @@ struct ReadError : std::runtime_error {
 };
 
 // ============================================================
-// remote_size
-// ============================================================
-
-template <typename T>
-constexpr std::size_t remote_size();
-
-// if Item is not specialized then we must have an invalid item in Layout
-template <typename Item>
-constexpr void apply_size_rule(Item, std::size_t &) {
-  // can't just write static_assert(false, ...) because we only want
-  // the compiler to evaluate this (and thus assert an error)
-  // when the template is instantiated
-  static_assert(
-    always_false<Item>::value, "Unsupported layout item in Layout<>"
-  );
-}
-
-template <std::size_t N>
-constexpr void apply_size_rule(Pad<N>, std::size_t &cursor) {
-  cursor += N;
-}
-
-template <std::size_t N>
-constexpr void apply_size_rule(Offset<N>, std::size_t &cursor) {
-  cursor = N;
-}
-
-template <auto Member>
-constexpr void apply_size_rule(Field<Member>, std::size_t &cursor) {
-  using member_type = typename member_pointer_traits<Member>::member_type;
-
-  if constexpr (has_remote_v<member_type>) {
-    cursor += remote_size<member_type>();
-  } else {
-    cursor += sizeof(member_type);
-  }
-}
-
-// calculate size of Layout<Items...>
-template <typename... Items>
-constexpr std::size_t remote_size(Layout<Items...>) {
-  std::size_t cursor = 0;
-  (apply_size_rule(Items{}, cursor), ...);
-  return cursor;
-}
-
-// calculate size of Remote<T>::layout
-template <typename T>
-constexpr std::size_t remote_size() {
-  return remote_size(remote_layout_t<T>{});
-}
-
-// ============================================================
 // read
 // ============================================================
 
+// forward declaration for recursive read in Field
 template <typename T>
-T read(const ReadRemote &read_remote, intptr_t base);
+intptr_t read(T &, const ReadRemote &, intptr_t);
 
 // if Item is not specialized then we must have an invalid item in Layout
 template <typename T, typename Item>
-void apply_read_rule(Item, T &, const ReadRemote &, intptr_t, std::size_t &) {
+intptr_t apply_read_rule(Item, T &, const ReadRemote &, intptr_t, intptr_t) {
   static_assert(always_false<Item>::value, "Unsupported layout item");
 }
 
 template <typename T, std::size_t N>
-void apply_read_rule(
-  Pad<N>, T &, const ReadRemote &, intptr_t, std::size_t &cursor
+intptr_t apply_read_rule(
+  Pad<N>, T &, const ReadRemote &, intptr_t, intptr_t cursor
 ) {
-  cursor += N;
+  return cursor + N;
 }
 
 template <typename T, std::size_t N>
-void apply_read_rule(
-  Offset<N>, T &, const ReadRemote &, intptr_t, std::size_t &cursor
+intptr_t apply_read_rule(
+  Offset<N>, T &, const ReadRemote &, intptr_t base, intptr_t
 ) {
-  cursor = N;
+  return base + N;
 }
 
 template <typename T, auto Member>
-void apply_read_rule(
+intptr_t apply_read_rule(
   Field<Member>,
   T &out,
   const ReadRemote &read_remote,
   intptr_t base,
-  std::size_t &cursor
+  intptr_t cursor
 ) {
   using member_type = typename member_pointer_traits<Member>::member_type;
-
-  auto remote_offset = base + static_cast<intptr_t>(cursor);
   auto &field = out.*Member;
-
   if constexpr (has_remote_v<member_type>) {
-    field = read<member_type>(read_remote, remote_offset);
-    cursor += remote_size<member_type>();
+    // read returns the new cursor after the nested struct
+    return read<member_type>(field, read_remote, cursor);
   } else {
-    if (!read_remote(&field, remote_offset, sizeof(member_type))) {
+    if (!read_remote(&field, cursor, sizeof(member_type))) {
       throw ReadError(
-        "failed to read field at remote offset " + std::to_string(cursor)
+        "failed to read at remote address " + std::to_string(cursor)
       );
     }
-    cursor += sizeof(member_type);
+    return cursor + sizeof(member_type);
   }
 }
 
 template <typename T, typename... Items>
-T read_layout(Layout<Items...>, const ReadRemote &read_remote, intptr_t base) {
-  T out{};
-  std::size_t cursor = 0;
-  (apply_read_rule(Items{}, out, read_remote, base, cursor), ...);
-  return out;
+intptr_t read_layout(
+  Layout<Items...>, T &out, const ReadRemote &read_remote, intptr_t base
+) {
+  // fold from first to last item
+  intptr_t cursor = base;
+  ((cursor = apply_read_rule(Items{}, out, read_remote, base, cursor)), ...);
+  return cursor;
 }
 
 template <typename T>
-T read(const ReadRemote &read_remote, intptr_t base) {
+intptr_t read(T &out, const ReadRemote &read_remote, intptr_t base) {
   static_assert(has_remote_v<T>, "Remote<T> must be specialized");
-  return read_layout<T>(remote_layout_t<T>{}, read_remote, base);
+  return read_layout<T>(remote_layout_t<T>{}, out, read_remote, base);
 }
 
 // ============================================================
@@ -230,14 +177,6 @@ struct Remote<Player> {
     Field<&Player::pos>>;
 };
 
-static_assert(remote_size<Pos>() == 16);
-static_assert(remote_size<Player>() == 32);
-
-// fails to compile: int not allowed in Layout (TODO test error message is ok)
-// struct Bad1 {};
-// template <> struct Remote<Bad1> { using layout = Layout<int>; };
-// static_assert(remote_size<Bad1>() == 0);
-
 int main() {
   // set up remote buffer
   ReadRemoteMock<40> buf{};
@@ -245,8 +184,9 @@ int main() {
   buf.write_int(26, 11);
   buf.write_int(34, 22);
   // fails to compile (TODO test error message is ok)
-  // read<Bad1>(buf, 0);
-  auto player = read<Player>(buf, 10);
+  // Bad1 bad1{}; read(bad1, buf, 0);
+  Player player{};
+  read(player, buf, 10);
   assert(player.health == 123);
   assert(player.pos.x == 11);
   assert(player.pos.y == 22);
