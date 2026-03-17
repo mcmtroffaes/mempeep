2d78f88d73dc2f6666a749da3c778d7060e9eade
Matthias C. M. Troffaes <matthias.troffaes@gmail.com>
2026-03-17 14:18:21 +0000

    Initial support for pointers.

--- cpp/test/example.cpp
+++ cpp/test/example.cpp
@@ -1,8 +1,9 @@
 #include <cassert>      // assert
-#include <cstdint>      // std::intptr_t
+#include <cstdint>      // std::intptr_t, ...
 #include <cstring>      // std::memcpy
+#include <expected>     // std::expected
 #include <functional>   // std::function
-#include <type_traits>  // std::void_t, ...
+#include <type_traits>  // std::is_same_v, ...
 
 namespace mempeep {
 
@@ -22,6 +23,22 @@ template <std::intptr_t N>
 struct Offset {
   static_assert(N > 0);
 };
+
+// Never null pointer, stores the pointee.
+template <auto MemberPtr>
+struct FieldRef {};  // TODO implement
+
+// Possible null pointer, stores the pointee via std::optional<T>.
+template <auto MemberPtr>
+struct FieldOptionalRef {};  // TODO implement
+
+// Never null pointer, stores remote pointer (intptr_t).
+template <auto MemberPtr>
+struct FieldPtr {};
+
+// Possible null pointer, stores remote pointer (intptr_t).
+template <auto MemberPtr>
+struct FieldOptionalPtr {};
 
 /**
  * @brief Defines a remote layout.
@@ -72,7 +89,21 @@ struct LayoutOf;
  * @param buffer Native destination buffer.
  * @return       Updated remote pointer.
  */
-using ReadMemory = std::function<intptr_t(intptr_t, intptr_t, void*)>;
+using MemoryRead = std::function<intptr_t(intptr_t, intptr_t, void*)>;
+
+using MemoryHandleNullPtr = std::function<intptr_t(intptr_t)>;
+
+template <std::size_t SizeOfPtr>
+struct Memory {
+  MemoryRead read = [](intptr_t cursor, intptr_t size, void* buffer) {
+    // simplest implementation: no reading, no error checking
+    return cursor + size;
+  };
+  MemoryHandleNullPtr handle_null_ptr = [](intptr_t cursor) {
+    // simplest implementation: do nothing
+    return cursor;
+  };
+};
 
 /**
  * @brief Check LayoutOf<T>::layout exists.
@@ -87,9 +118,9 @@ template <typename T>
 using layout_t = typename LayoutOf<T>::layout;
 
 // Forward declaration to support recursive Layout reading.
-template <typename T>
+template <std::size_t SizeOfPtr, typename T>
   requires HasLayout<T>
-intptr_t read(const ReadMemory& read_memory, intptr_t base, T& target);
+intptr_t read(const Memory<SizeOfPtr>& memory, intptr_t base, T& target);
 
 namespace detail {
 
@@ -106,33 +137,81 @@ struct member_pointer_traits<MemberPtr> {
   using member_type = T;
 };
 
-// If Item is not specialized then we must have an invalid item in Layout.
-template <typename T, typename Item>
-intptr_t read_layout_item(Item, const ReadMemory&, intptr_t, intptr_t, T&) {
+template <std::size_t Size>
+struct signed_int;
+
+template <>
+struct signed_int<1> {
+  using type = std::int8_t;
+};
+
+template <>
+struct signed_int<2> {
+  using type = std::int16_t;
+};
+
+template <>
+struct signed_int<4> {
+  using type = std::int32_t;
+};
+
+template <>
+struct signed_int<8> {
+  using type = std::int64_t;
+};
+
+template <std::size_t Size>
+using signed_int_t = typename signed_int<Size>::type;
+
+template <std::size_t SizeOfPtr>
+intptr_t read_optional_ptr(
+  const MemoryRead& memory_read, intptr_t cursor, intptr_t& target
+) {
+  signed_int_t<SizeOfPtr> ptr{};
+  static_assert(sizeof(ptr) == SizeOfPtr);
+  static_assert(sizeof(intptr_t) >= SizeOfPtr);
+  cursor = memory_read(cursor, sizeof(ptr), &ptr);
+  target = static_cast<intptr_t>(ptr);
+  return cursor;
+}
+
+template <std::size_t SizeOfPtr>
+intptr_t read_ptr(
+  const MemoryRead& memory_read,
+  const MemoryHandleNullPtr& memory_handle_null_ptr,
+  intptr_t cursor,
+  intptr_t& target
+) {
+  return memory_handle_null_ptr(
+    read_optional_ptr<SizeOfPtr>(memory_read, cursor, target)
+  );
+}
+
+template <typename Item, std::size_t SizeOfPtr, typename T>
+intptr_t read_layout_item(
+  Item, const Memory<SizeOfPtr>&, intptr_t, intptr_t, T&
+) {
   static_assert(!std::is_same_v<Item, Item>, "Unsupported layout item");
 }
 
-// Specialization for Pad<N>.
-template <typename T, std::size_t N>
+template <std::size_t N, std::size_t SizeOfPtr, typename T>
 intptr_t read_layout_item(
-  Pad<N>, const ReadMemory& read_memory, intptr_t, intptr_t cursor, T&
+  Pad<N>, const Memory<SizeOfPtr>& memory, intptr_t, intptr_t cursor, T&
 ) {
-  return read_memory(cursor, N, nullptr);
+  return memory.read(cursor, N, nullptr);
 }
 
-// Specialization for Offset<N>.
-template <typename T, std::size_t N>
+template <std::size_t N, std::size_t SizeOfPtr, typename T>
 intptr_t read_layout_item(
-  Offset<N>, const ReadMemory& read_memory, intptr_t base, intptr_t, T&
+  Offset<N>, const Memory<SizeOfPtr>& memory, intptr_t base, intptr_t, T&
 ) {
-  return read_memory(base, N, nullptr);
+  return memory.read(base, N, nullptr);
 }
 
-// Specialization for Field<MemberPtr>.
-template <typename T, auto MemberPtr>
+template <auto MemberPtr, std::size_t SizeOfPtr, typename T>
 intptr_t read_layout_item(
   Field<MemberPtr>,
-  const ReadMemory& read_memory,
+  const Memory<SizeOfPtr>& memory,
   intptr_t base,
   intptr_t cursor,
   T& target
@@ -140,20 +219,53 @@ intptr_t read_layout_item(
   using member_type = typename member_pointer_traits<MemberPtr>::member_type;
   auto& field = target.*MemberPtr;
   if constexpr (HasLayout<member_type>) {
-    return read(read_memory, cursor, field);
+    return read(memory, cursor, field);
   } else {
-    return read_memory(cursor, sizeof(field), &field);
+    return memory.read(cursor, sizeof(field), &field);
   }
 }
 
-template <typename T, typename... Items>
+template <auto MemberPtr, std::size_t SizeOfPtr, typename T>
+intptr_t read_layout_item(
+  FieldOptionalPtr<MemberPtr>,
+  const Memory<SizeOfPtr>& memory,
+  intptr_t base,
+  intptr_t cursor,
+  T& target
+) {
+  using member_type = typename member_pointer_traits<MemberPtr>::member_type;
+  static_assert(
+    std::is_same_v<member_type, intptr_t>,
+    "FieldOptionalPtr needs member type to be intptr_t"
+  );
+  auto& field = target.*MemberPtr;
+  return read_optional_ptr<SizeOfPtr>(memory.read, cursor, field);
+}
+
+template <auto MemberPtr, std::size_t SizeOfPtr, typename T>
+intptr_t read_layout_item(
+  FieldPtr<MemberPtr>,
+  const Memory<SizeOfPtr>& memory,
+  intptr_t base,
+  intptr_t cursor,
+  T& target
+) {
+  using member_type = typename member_pointer_traits<MemberPtr>::member_type;
+  static_assert(
+    std::is_same_v<member_type, intptr_t>,
+    "FieldPtr needs member type to be intptr_t"
+  );
+  auto& field = target.*MemberPtr;
+  return read_ptr(memory, cursor, field);
+}
+
+template <typename... Items, std::size_t SizeOfPtr, typename T>
 intptr_t read_layout(
-  Layout<Items...>, const ReadMemory& read_memory, intptr_t base, T& target
+  Layout<Items...>, const Memory<SizeOfPtr>& memory, intptr_t base, T& target
 ) {
   intptr_t cursor = base;
   // fold from first to last item
-  ((cursor = read_layout_item(Items{}, read_memory, base, cursor, target)),
-   ...);
+  ((cursor = read_layout_item(Items{}, memory, base, cursor, target)), ...);
   return cursor;
 }
 
@@ -178,10 +290,10 @@ intptr_t read_layout(
  * @param target             Native destination.
  * @return                   The outcome of the final read_memory call.
  */
-template <typename T>
+template <std::size_t SizeOfPtr, typename T>
   requires HasLayout<T>
-intptr_t read(const ReadMemory& read_memory, intptr_t base, T& target) {
-  return detail::read_layout(layout_t<T>{}, read_memory, base, target);
+intptr_t read(const Memory<SizeOfPtr>& memory, intptr_t base, T& target) {
+  return detail::read_layout(layout_t<T>{}, memory, base, target);
 }
 
 }  // namespace mempeep
@@ -193,7 +305,8 @@ intptr_t read(const ReadMemory& read_memory, intptr_t base, T& target) {
 #include <iostream>
 
 template <intptr_t N>
-struct ReadMemoryMock {
+struct MemoryReadMock {
+  static_assert(N > 0);
   char data[N]{};
 
   intptr_t operator()(intptr_t cursor, intptr_t size, void* buffer) const {
@@ -203,7 +316,8 @@ struct ReadMemoryMock {
     return cursor + size;
   }
 
-  void write_int(intptr_t offset, int value) {
+  template <typename T>
+  void write(intptr_t offset, T value) {
     assert(sizeof(value) <= N && offset >= 0 && offset <= N - sizeof(value));
     std::memcpy(data + offset, &value, sizeof(value));
   }
@@ -216,6 +330,7 @@ struct Pos {
 struct Player {
   int health;
   Pos pos;
+  intptr_t target_ptr;
 };
 
 template <>
@@ -229,19 +344,27 @@ struct mempeep::LayoutOf<Player> {
     Offset<8>,
     Field<&Player::health>,
     Offset<16>,
-    Field<&Player::pos>>;
+    Field<&Player::pos>,
+    FieldOptionalPtr<&Player::target_ptr>>;
 };
 
 int main() {
-  ReadMemoryMock<48> buf{};
-  buf.write_int(18, 123);
-  buf.write_int(26, 11);
-  buf.write_int(34, 22);
+  MemoryReadMock<64> memory_read{};
+  memory_read.write(18, int32_t(123));
+  memory_read.write(26, int32_t(11));
+  memory_read.write(34, int32_t(22));
+  memory_read.write(42, int16_t(4));
+  auto handle_null_ptr = [](intptr_t cursor) {
+    assert(false);
+    return cursor;
+  };
+  auto memory = mempeep::Memory<2>{memory_read, handle_null_ptr};
 
   Player player{};
-  mempeep::read(buf, 10, player);
+  mempeep::read(memory, 10, player);
 
   assert(player.health == 123);
   assert(player.pos.x == 11);
   assert(player.pos.y == 22);
+  assert(player.target_ptr == 4);
 }
