4367185cc571c9a8eeae57f3646b3b6e8ede99b4
Matthias C. M. Troffaes <matthias.troffaes@gmail.com>
2026-03-20 18:51:04 +0000

    Rename.

--- cpp/test/example.cpp
+++ cpp/test/example.cpp
@@ -354,7 +354,7 @@ template <
   T&,
   Tracer& tracer
 ) {
-  [[maybe_unused]] auto _
+  [[maybe_unused]] auto scope
     = make_scope(tracer, address, std::format("pad(0x{:X})", N));
   return safe_offset(address, N, tracer);
 }
@@ -372,7 +372,7 @@ template <
   T&,
   Tracer& tracer
 ) {
-  [[maybe_unused]] auto _
+  [[maybe_unused]] auto scope
     = make_scope(tracer, address, std::format("offset(0x{:X})", N));
   return safe_offset(base, N, tracer);
 }
@@ -387,7 +387,7 @@ template <auto M, IsMemoryRead MemoryRead, IsReadable T, IsTracer Tracer>
   T& target,
   Tracer& tracer
 ) {
-  [[maybe_unused]] auto _ = make_scope(tracer, address, member_name<M>());
+  [[maybe_unused]] auto scope = make_scope(tracer, address, member_name<M>());
   auto& field = target.*M;
   return read(memory_read, address, field, tracer);
 }
@@ -402,7 +402,7 @@ template <auto M, IsMemoryRead MemoryRead, IsReadable T, IsTracer Tracer>
   T& target,
   Tracer& tracer
 ) {
-  [[maybe_unused]] auto _ = make_scope(tracer, address, member_name<M>());
+  [[maybe_unused]] auto scope = make_scope(tracer, address, member_name<M>());
   pointer_type_t<MemoryRead> target_ptr{};
   if (!memory_read(address, sizeof(target_ptr), &target_ptr)) return {};
   auto& field = target.*M;
@@ -421,7 +421,7 @@ template <auto M, IsMemoryRead MemoryRead, IsReadable T, IsTracer Tracer>
   T& target,
   Tracer& tracer
 ) {
-  [[maybe_unused]] auto _ = make_scope(tracer, address, member_name<M>());
+  [[maybe_unused]] auto scope = make_scope(tracer, address, member_name<M>());
   pointer_type_t<MemoryRead> target_ptr{};
   if (!memory_read(address, sizeof(target_ptr), &target_ptr)) return {};
   if (target_ptr) {
@@ -445,7 +445,7 @@ template <auto M, IsMemoryRead MemoryRead, IsReadable T, IsTracer Tracer>
   T& target,
   Tracer& tracer
 ) {
-  [[maybe_unused]] auto _ = make_scope(tracer, address, member_name<M>());
+  [[maybe_unused]] auto scope = make_scope(tracer, address, member_name<M>());
   pointer_type_t<MemoryRead> target_ptr{};
   if (!memory_read(address, sizeof(target_ptr), &target_ptr)) return {};
   auto& field = target.*M;
