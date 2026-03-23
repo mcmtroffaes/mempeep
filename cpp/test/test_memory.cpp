42bc0233491d355c6d9694bb9f8d1b705eb70cdd
Matthias C. M. Troffaes <matthias.troffaes@gmail.com>
2026-03-23 20:25:02 +0000

    Extend tests.

--- cpp/test/test_memory.cpp
+++ cpp/test/test_memory.cpp
@@ -4,7 +4,7 @@
 
 struct Struct1 {};
 
-template<typename T>
+template <typename T>
 struct Struct2 {
   using address_type = T;
 };
@@ -18,7 +18,17 @@ struct Struct3 {
 
 static_assert(!mempeep::IsMemoryReader<Struct1>);
 static_assert(!mempeep::IsMemoryReader<Struct2<uint32_t>>);
-static_assert(!mempeep::IsMemoryReader<Struct3<int32_t>>);  // signed -> no...
+static_assert(!mempeep::IsMemoryReader<Struct3<bool>>);
+static_assert(!mempeep::IsMemoryReader<Struct3<char>>);
+static_assert(!mempeep::IsMemoryReader<Struct3<short>>);    // signed...
+static_assert(!mempeep::IsMemoryReader<Struct3<int>>);      // signed...
+static_assert(!mempeep::IsMemoryReader<Struct3<int32_t>>);  // signed...
+static_assert(mempeep::IsMemoryReader<Struct3<uint8_t>>);
+static_assert(mempeep::IsMemoryReader<Struct3<uint16_t>>);
 static_assert(mempeep::IsMemoryReader<Struct3<uint32_t>>);
+static_assert(mempeep::IsMemoryReader<Struct3<uint64_t>>);
+static_assert(mempeep::IsMemoryReader<Struct3<uintptr_t>>);
+static_assert(mempeep::IsMemoryReader<Struct3<uintmax_t>>);
+static_assert(mempeep::IsMemoryReader<Struct3<std::size_t>>);
 
 int main() { return 0; };
