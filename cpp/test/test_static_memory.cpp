615a81e5d83651d68da85eedfc909fbe51b9c904
Matthias C. M. Troffaes <matthias.troffaes@gmail.com>
2026-03-25 16:35:06 +0000

    Renames.

--- cpp/test/test_static_memory.cpp
+++ cpp/test/test_static_memory.cpp
@@ -3,33 +3,33 @@
 #include <cstdint>  // std::int32_t, ...
 #include <mempeep/memory.hpp>
 
-struct Struct1 {};
+struct Obj1 {};
 
 template <typename T>
-struct Struct2 {
+struct Obj2 {
   using address_type = T;
 };
 
 template <typename T>
-struct Struct3 {
+struct Obj3 {
   using address_type = T;
 
   bool operator()(T, std::size_t, void*) { return false; };
 };
 
-static_assert(!mempeep::IsMemoryReader<Struct1>);
-static_assert(!mempeep::IsMemoryReader<Struct2<std::uint32_t>>);
-static_assert(!mempeep::IsMemoryReader<Struct3<bool>>);
-static_assert(!mempeep::IsMemoryReader<Struct3<char>>);
-static_assert(!mempeep::IsMemoryReader<Struct3<short>>);         // signed...
-static_assert(!mempeep::IsMemoryReader<Struct3<int>>);           // signed...
-static_assert(!mempeep::IsMemoryReader<Struct3<std::int32_t>>);  // signed...
-static_assert(mempeep::IsMemoryReader<Struct3<std::uint8_t>>);
-static_assert(mempeep::IsMemoryReader<Struct3<std::uint16_t>>);
-static_assert(mempeep::IsMemoryReader<Struct3<std::uint32_t>>);
-static_assert(mempeep::IsMemoryReader<Struct3<std::uint64_t>>);
-static_assert(mempeep::IsMemoryReader<Struct3<std::uintptr_t>>);
-static_assert(mempeep::IsMemoryReader<Struct3<std::uintmax_t>>);
-static_assert(mempeep::IsMemoryReader<Struct3<std::size_t>>);
+static_assert(!mempeep::IsMemoryReader<Obj1>);
+static_assert(!mempeep::IsMemoryReader<Obj2<std::uint32_t>>);
+static_assert(!mempeep::IsMemoryReader<Obj3<bool>>);
+static_assert(!mempeep::IsMemoryReader<Obj3<char>>);
+static_assert(!mempeep::IsMemoryReader<Obj3<short>>);         // signed...
+static_assert(!mempeep::IsMemoryReader<Obj3<int>>);           // signed...
+static_assert(!mempeep::IsMemoryReader<Obj3<std::int32_t>>);  // signed...
+static_assert(mempeep::IsMemoryReader<Obj3<std::uint8_t>>);
+static_assert(mempeep::IsMemoryReader<Obj3<std::uint16_t>>);
+static_assert(mempeep::IsMemoryReader<Obj3<std::uint32_t>>);
+static_assert(mempeep::IsMemoryReader<Obj3<std::uint64_t>>);
+static_assert(mempeep::IsMemoryReader<Obj3<std::uintptr_t>>);
+static_assert(mempeep::IsMemoryReader<Obj3<std::uintmax_t>>);
+static_assert(mempeep::IsMemoryReader<Obj3<std::size_t>>);
 
 int main() { return 0; };
